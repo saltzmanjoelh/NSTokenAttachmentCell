43ffb61c5f94de53d3e0ec441d2f767b7f24915b
Joel Saltzman <talk@joelsaltzman.com>
2020-08-27 10:45:35 -0700

    Reverted project

--- Package.swift
+++ Package.swift
@@ -1,14 +1,8 @@
-// swift-tools-version:5.2
+// swift-tools-version:4.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
-    name: "NSTokenAttachmentCell",
-    products: [
-        .library(name: "NSTokenAttachmentCell", targets: ["NSTokenAttachmentCell"]),
-    ],
-    targets: [
-        .target(name: "NSTokenAttachmentCell"),
-    ]
+    name: "NSTokenAttachmentCell"
 )
